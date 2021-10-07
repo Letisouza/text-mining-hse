f84013f4b4f6023aae570955bf0fd2c708e8c766
Leticia Souza <leticiasouzaufpe@gmail.com>
2021-10-07 11:15:03 -0300

    Semana 1

---
+++ scripts/teste.R
@@ -0,0 +1,15 @@
+
+# teste de seção -------------------------------------------------------------------
+
+## subseção: Sempre escreva strings entre aspas -------------------------------------
+
+print("Welcome to the course!")
+
+
+# outra seção: variáveis -----------------------------------------------------
+
+# Criando variáveis que guardam texto
+welcome_variable <- "Welcome to the couse!"
+
+print(welcome_variable)
+
